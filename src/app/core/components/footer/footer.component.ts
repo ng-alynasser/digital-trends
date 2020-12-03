a18d0a66b04bced6e4b1f170fb6132fa3ab9016a
Aly Nasser <ng.alynasser@gmail.com>
2020-12-03 14:21:55 +0200

    feat: footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faWhatsapp,
+  faLinkedinIn,
+  faGoogle,
+  faFacebookF,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  faEmail = faEnvelope;
+  faWhatsapp = faWhatsapp;
+  faMap = faMapMarkerAlt;
+  faLinkedIn = faLinkedinIn;
+  faGoogle = faGoogle;
+  faFacebook = faFacebookF;
+  faTwitter = faTwitter;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DTLayoutDirection } from '../../constants';
 import { DirectionService } from '../../services/direction.service';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -9,6 +9,7 @@ import { StateService } from '../../services/state.service';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
   faBars = faBars;
