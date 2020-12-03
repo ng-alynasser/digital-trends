408b9074ef80ed4b238bd94a5208d4df06e49114
Aly Nasser <ng.alynasser@gmail.com>
2020-12-03 15:20:02 +0200

    feat: home-header

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { SharedModule } from '../../shared/shared.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [SharedModule, HomeRoutingModule, FlexLayoutModule],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  exports: [CommonModule, FormsModule, RouterModule],
+  exports: [CommonModule, FormsModule, RouterModule, TranslateModule],
 })
 export class SharedModule {}
