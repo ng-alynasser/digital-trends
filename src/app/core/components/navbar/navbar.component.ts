e0a227b7753dc9cdff5064e1ed165441961fda1f
Aly Nasser <ng.alynasser@gmail.com>
2020-12-07 03:30:28 +0200

    feat: consultancy

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { DTLayoutDirection } from '../../constants';
 import { DirectionService } from '../../services/direction.service';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -11,13 +11,15 @@ import { StateService } from '../../services/state.service';
   styleUrls: ['./navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   faBars = faBars;
 
   constructor(
     private readonly directionService: DirectionService,
     private readonly stateService: StateService
   ) {}
+
+  ngOnInit(): void {}
 
   toggleLanguage(): void {
     this.directionService.isRtl()
