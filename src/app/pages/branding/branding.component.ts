5346c4df1fe4a07cbfa104b7092a7e63496501a1
Aly Nasser <ng.alynasser@gmail.com>
2020-12-13 16:45:12 +0200

    fix: branding res

--- src/app/pages/branding/branding.component.ts
+++ src/app/pages/branding/branding.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DirectionService } from '../../core/services/direction.service';
 
 @Component({
   selector: 'app-branding',
   templateUrl: './branding.component.html',
-  styleUrls: ['./branding.component.scss']
+  styleUrls: ['./branding.component.scss'],
 })
 export class BrandingComponent implements OnInit {
+  public currentDir = 'rtl';
 
-  constructor() { }
+  constructor(private readonly directionService: DirectionService) {}
 
   ngOnInit(): void {
+    this.directionService
+      .onDirectionChange()
+      .subscribe((dir) => (this.currentDir = dir));
   }
-
 }
